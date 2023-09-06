906538791f28ff9310c3e63070505fdeeb99cb47
Devansh Mandloi <131014087+Trividh15@users.noreply.github.com>
2023-09-06 23:12:37 +0530

    Add files via upload

---
+++ Arrays/arrays1.cpp
@@ -0,0 +1,26 @@
+//Que1} Find the largest element of the array
+
+//Approach 1: Brute Force -> Using Sorting
+
+//Appoach 2: Optimal Method-> Using loop
+
+
+#include<iostream>
+int main ()
+{
+  int arr[10]={1,2,3,15,31,12,22,8,9,10}, temp;
+  int max = arr[0];
+
+  for(int i = 1; i<10; i++)
+  {
+    if(arr[i]>max)
+    {
+      temp = max;
+      max = arr[i];
+      arr[i] = max;
+    }
+  }
+
+  std::cout<<"The largest element of the array is: "<<max; 
+  return 0;
+}
