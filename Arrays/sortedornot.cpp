e929c898f9fe7a2dd499559d046da71c2d443e99
Devansh Mandloi <131014087+Trividh15@users.noreply.github.com>
2023-09-08 22:31:36 +0530

    Add files via upload

---
+++ Arrays/sortedornot.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+
+bool is_sorted(int arr[], int n)
+{
+  for (int i = 1; i < n; i++)
+  {
+    if (arr[i] >= arr[i - 1])
+    {
+      
+    }
+    else
+    {
+      return false;
+    }
+  }
+  return true;
+}
+
+int main()
+{
+  int arr[] = {2,4,1,5,7,23,7};
+  int n = sizeof(arr)/sizeof(arr[0]);
+
+  if(is_sorted)
+  {
+    std::cout<<"The array is sorted.";
+  }
+  else{
+  std::cout<<"The array is not sorted.";
+  }
+
+  return 0;
+}
